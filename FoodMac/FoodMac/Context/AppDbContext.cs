b4529a6ecb2da4b267f50c78ccf207de61b1138b
Abel Silva <abel2901@gmail.com>
2023-01-06 08:09:11 -0300

    Configura o Entity Framework Core

---
+++ FoodMac/FoodMac/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FoodMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodMac.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Food> Foods { get; set; }
+    }
+}
