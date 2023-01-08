34d82545a3bb76735d49a104569efbb48c58172f
Abel Silva <abel2901@gmail.com>
2023-01-08 20:00:03 -0300

    Criando anotações

--- FoodMac/FoodMac/Models/Category.cs
+++ FoodMac/FoodMac/Models/Category.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FoodMac.Models
 {
+    [Table("Categories")]
     public class Category
     {
+        [Key]
         public int CategoryId { get; set; }
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name = "Nome")]
         public string CategoryName { get; set; }
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Nome")]
         public string Description { get; set; }
         public List<Food> Foods { get; set; }
     }
--- FoodMac/FoodMac/Models/Food.cs
+++ FoodMac/FoodMac/Models/Food.cs
@@ -1,16 +1,38 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FoodMac.Models
 {
+    [Table("Foods")]
     public class Food
     {
+        [Key]
         public int FoodId { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado")]
+        [Display(Name = "Nome do lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no minimo {1} e no máximo {2}")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado")]
+        [Display(Name = "Nome do lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string ShortDescription { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado")]
+        [Display(Name = "Nome do lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DetailsDescription { get; set; }
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 a 999")]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
+        [Display(Name = "Preferido ?")]
         public bool IsFavoriteFood { get; set; }
+        [Display(Name ="InStock")]
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
