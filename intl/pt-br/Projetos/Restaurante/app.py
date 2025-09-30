fdad01c731af421c9f24238f80ea0d349dc16d06
Abel Silva <abel2901@gmail.com>
2025-09-30 16:16:19 -0300

    Cria projeto restaurante

---
+++ intl/pt-br/Projetos/Restaurante/app.py
@@ -0,0 +1,70 @@
+import os
+
+restaurantes = ['Pizza', 'Sushi']
+
+def exibir_nome_do_programa():
+    print('Sabor Express') 
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    exibir_subtitulo('Finalizando o app\n')
+
+def voltar_ao_menu_principal():
+    input('\nDigite um tecla para voltar ao menu principal ')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida!')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastra_novo_restaurante():
+    os.system('cls')
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!!')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    voltar_ao_menu_principal()
+
+
+def escolher_opcao(): 
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
+
+        if opcao_escolhida == 1:
+            cadastra_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
