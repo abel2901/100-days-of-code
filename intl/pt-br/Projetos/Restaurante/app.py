210bdc5bf7350a57d0bf2d3732c5fe9f93ba23bf
Abel Silva <abel2901@gmail.com>
2025-10-01 10:50:20 -0300

    day-2/ativando restaurantes

--- intl/pt-br/Projetos/Restaurante/app.py
+++ intl/pt-br/Projetos/Restaurante/app.py
@@ -49,6 +49,21 @@ def listar_restaurantes():
         print(f' - {nome_do_restaurante} | {categoria} | {ativo}')
     voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterando estad do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante foi desativado com sucesso'
+            print(mensagem)
+        
+    if not restaurante_encontrado:
+     print('O restaurante não foi encontrado')
+
 
 def escolher_opcao(): 
     try:
@@ -60,7 +75,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
